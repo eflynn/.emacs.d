20fa04d4d2eaa79d4c02e746caafa8a058867a9a
Evan Flynn <evan.flynn@gmail.com>
2012-11-01 16:06:43 -0400

    remove color theme version for easy reading

--- init.el
+++ init.el
@@ -1,4 +1,4 @@
-(setq load-path (cons "~/.emacs.d/color-theme-6.6.0" load-path))
+(setq load-path (cons "~/.emacs.d/color-theme" load-path))
 (require 'color-theme)
 
 (eval-after-load "color-theme"
