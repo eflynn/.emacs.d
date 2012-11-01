7ee7d2b817783b284197bc1319b4368d08975ebf
Evan Flynn <evan.flynn@gmail.com>
2012-11-01 16:41:34 -0300

    Update init.el

--- init.el
+++ init.el
@@ -1,6 +1,3 @@
-(menu-bar-mode -1)
-(tool-bar-mode -1)
-
 (setq load-path (cons "~/.emacs.d/color-theme-6.6.0" load-path))
 (require 'color-theme)
 
@@ -8,3 +5,10 @@
 		'(progn
 		   (color-theme-initialize)
 		   (color-theme-hober)))
+
+(color-theme-midnight)
+(ido-mode 1)
+(menu-bar-mode -1)
+(tool-bar-mode -1)
+(windmove-default-keybindings)
+(setq make-backup-files nil)
