aa409cb4c60962d298fab1b3c17f2a40e39c2355
Evan Flynn <evan.flynn@gmail.com>
2013-08-13 18:15:34 -0400

    Add magit support

--- init.el
+++ init.el
@@ -1,5 +1,7 @@
 (add-to-list 'load-path "~/.emacs.d/")
+(add-to-list 'load-path "~/.emacs.d/magit/")
 
+(require 'magit)
 (require 'color-theme)
 
 (eval-after-load "color-theme"
@@ -29,6 +31,7 @@
 (global-set-key (kbd "C-r") 'isearch-backward-regexp)
 (global-set-key (kbd "C-M-s") 'isearch-forward)
 (global-set-key (kbd "C-M-r") 'isearch-backward)
+(global-set-key (kbd "C-x g") 'magit-status)
 
 (show-paren-mode 1)
 (setq-default indent-tabs-mode nil)
