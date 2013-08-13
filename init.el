8d9da88ef0337acc54157cfe899d81412f308c55
Evan Flynn <evan.flynn@gmail.com>
2013-08-13 16:22:33 -0400

    Refactor init.el file and move color-theme

--- init.el
+++ init.el
@@ -1,23 +1,9 @@
-(setq load-path (cons "~/.emacs.d/color-theme" load-path))
+(add-to-list 'load-path "~/.emacs.d/")
+
 (require 'color-theme)
 
 (eval-after-load "color-theme"
   '(progn
      (color-theme-initialize)
-     (color-theme-hober)))
+     (color-theme-midnight)))
 
-(color-theme-midnight)
-(ido-mode 1)
-
-;; Turn off menus, toolbar, and scrollbars.
-(menu-bar-mode 0)
-
-(if window-system
-    (progn
-      (tool-bar-mode 0)
-      (scroll-bar-mode 0)))
-
-(windmove-default-keybindings)
-(setq make-backup-files nil
-      css-indent-offset 2)
-(server-start)
