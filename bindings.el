ba22bd4201da849742f1d0ba8fce42450a13384a
Evan Flynn <evan.flynn@gmail.com>
2013-08-15 15:48:42 -0400

    Spin off bindings into new file

---
+++ bindings.el
@@ -0,0 +1,13 @@
+;; Bindings
+
+(global-set-key (kbd "M-/") 'hippie-expand)
+(global-set-key (kbd "C-x C-b") 'ibuffer)
+
+(global-set-key (kbd "C-s") 'isearch-forward-regexp)
+(global-set-key (kbd "C-r") 'isearch-backward-regexp)
+(global-set-key (kbd "C-M-s") 'isearch-forward)
+(global-set-key (kbd "C-M-r") 'isearch-backward)
+
+(global-set-key (kbd "C-x g") 'magit-status)
+
+(provide 'bindings)
--- init.el
+++ init.el
@@ -3,6 +3,7 @@
 
 (require 'magit)
 (require 'color-theme)
+(require 'bindings)
 
 (eval-after-load "color-theme"
   '(progn
@@ -23,15 +24,6 @@
 
 (require 'saveplace)
 (setq-default save-place t)
-
-(global-set-key (kbd "M-/") 'hippie-expand)
-(global-set-key (kbd "C-x C-b") 'ibuffer)
-
-(global-set-key (kbd "C-s") 'isearch-forward-regexp)
-(global-set-key (kbd "C-r") 'isearch-backward-regexp)
-(global-set-key (kbd "C-M-s") 'isearch-forward)
-(global-set-key (kbd "C-M-r") 'isearch-backward)
-(global-set-key (kbd "C-x g") 'magit-status)
 
 (column-number-mode 1)
 (windmove-default-keybindings)
