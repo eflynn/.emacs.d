769d7ea880f04906672650f15d5206c4614d8cd6
Evan Flynn <evan.flynn@gmail.com>
2013-10-14 20:48:27 -0400

    Make new appearance.el file

--- init.el
+++ init.el
@@ -1,18 +1,9 @@
 
 (add-to-list 'load-path (concat user-emacs-directory "lisp-personal"))
 
+(require 'appearance)
 (require 'load-path)
 (require 'languages)
-
-;; Turn off toolbars, menus, tooltips, and left fringe.
-(menu-bar-mode 0)
-
-(when (display-graphic-p)
-  (tool-bar-mode 0)
-  (scroll-bar-mode 0)
-  (tooltip-mode 0)
-  (fringe-mode '(0 . 8)))
-
 (require 'key-bindings)
 (autoload 'magit-status "magit")
 
@@ -44,9 +35,6 @@
                                                "backups")))
       inhibit-startup-message t)
 
-(when (eq system-type 'windows-nt)
-  ;; Set default font
-  (add-to-list 'default-frame-alist '(font . "Consolas-10")))
 
 (add-to-list 'custom-theme-load-path
              (concat user-emacs-directory "themes"))
