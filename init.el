2c2009739aea8685326443a17b20506c79ae5612
Evan Flynn <evan.flynn@gmail.com>
2013-10-15 10:24:57 -0400

    Alphabetize variable names

--- init.el
+++ init.el
@@ -9,17 +9,16 @@
 (require 'multiple-cursors)
 (require 'uniquify)
 
-(setf (default-value 'indent-tabs-mode) nil
+(setf frame-title-format "%b - %F"
+      ido-enable-flex-matching t
+      (default-value 'indent-tabs-mode) nil
+      inhibit-startup-message t
+      make-backup-files nil
+      mouse-yank-at-point t
+      save-interprogram-paste-before-kill t
       uniquify-buffer-name-style 'forward
       x-select-enable-clipboard t
-      frame-title-format "%b - %F"
-      x-select-enable-primary t
-      save-interprogram-paste-before-kill t
-      apropos-do-all t
-      ido-enable-flex-matching t
-      mouse-yank-at-point t
-      make-backup-files nil
-      inhibit-startup-message t)
+      x-select-enable-primary t)
 
 (server-start)
 (ido-mode 1)
