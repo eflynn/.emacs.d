7ca2a1e1c2e36d3de06753ad13e6c4ca8de039cb
Evan Flynn <evan.flynn@gmail.com>
2013-10-24 12:52:01 -0400

    Change how cruft is turned off.
    Remove display-graphic-p check. If emacs is started from a tty, and then
    you eval (server-start), and open a new frame, that frame should not have
    the tool bars. So this will turn them off unilaterally.
    
    Fringes are now set as frame parameters in default-frame-alist.
    
    Also using window-system-default-frame-alist might be cleaner than
    checking system-type.

--- lisp-personal/appearance.el
+++ lisp-personal/appearance.el
@@ -1,18 +1,17 @@
 ;; Turn off toolbars, menus, tooltips, and left fringe.
 (menu-bar-mode 0)
+(tool-bar-mode 0)
+(tooltip-mode 0)
 
-(when (display-graphic-p)
-  (tool-bar-mode 0)
-  (scroll-bar-mode 0)
-  (tooltip-mode 0)
-  (fringe-mode '(0 . 8)))
+(setq default-frame-alist
+      '((left-fringe . 0)
+        (right-fringe . 8)))
 
 ;; Set theme
 (load-theme 'wheatgrass t)
 
-(when (eq system-type 'windows-nt)
-  ;; Set default font when on windows
-  (add-to-list 'default-frame-alist '(font . "Consolas-10")))
+(setq window-system-default-frame-alist
+      '((w32 . ((font . "Consolas-10")))))
 
 (set-face-background 'fringe "black")
 
