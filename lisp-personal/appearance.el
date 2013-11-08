87e6f97a4e18bdb8b3246ff718d0addc019e8e67
Evan Flynn <evan.flynn@gmail.com>
2013-11-08 13:57:35 -0500

    change appearance configuration

--- lisp-personal/appearance.el
+++ lisp-personal/appearance.el
@@ -1,10 +1,13 @@
 ;; Turn off toolbars, menus, tooltips, and left fringe.
-(menu-bar-mode 0)
-(tool-bar-mode 0)
+(require 'tool-bar)
+(require 'tooltip)
+
 (tooltip-mode 0)
+(tool-bar-mode 0)
 
 (setq default-frame-alist
-      '((left-fringe . 0)
+      '((menu-bar-lines . 0)
+        (left-fringe . 0)
         (right-fringe . 8)))
 
 ;; Set theme
