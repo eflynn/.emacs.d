6a75d6e8423b4120f9d2f10aa27f00fc228065e8
Evan Flynn <evan.flynn@gmail.com>
2013-11-21 16:29:25 -0500

    Update keybindings

--- init.d/key-bindings.el
+++ init.d/key-bindings.el
@@ -3,7 +3,7 @@
 (global-set-key (kbd "M-/") 'hippie-expand)
 (global-set-key (kbd "C-x C-b") 'ibuffer)
 
-(global-set-key (kbd "C-s") 'isearch-forward-regexp)
+(global-set-key (kbd "C-f") 'isearch-forward-regexp)
 (global-set-key (kbd "C-r") 'isearch-backward-regexp)
 (global-set-key (kbd "C-M-s") 'isearch-forward)
 (global-set-key (kbd "C-M-r") 'isearch-backward)
@@ -19,3 +19,7 @@
 
 ;; Smex
 (global-set-key (kbd "M-x") 'smex)
+
+;; Sacrilegious key bindings
+(global-unset-key (kbd "C-x C-s"))
+(global-set-key (kbd "C-s") 'save-buffer)
